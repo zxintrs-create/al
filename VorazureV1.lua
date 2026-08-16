57caa45264a27c1abd5b29463a4da26b44c129bb
HEAVELYNE  ART <zxintrs@gmail.com>
2026-08-16 09:03:38 +0700

    Update VorazureV1.lua
    Signed-off-by: HEAVELYNE  ART <zxintrs@gmail.com>

--- VorazureV1.lua
+++ VorazureV1.lua
@@ -4,25 +4,24 @@ ScreenGui.Name="VZMenu"
 ScreenGui.ResetOnSpawn=false
 ScreenGui.ZIndexBehavior=Enum.ZIndexBehavior.Sibling
 ScreenGui.Parent=game:GetService("CoreGui")
-local OpenButton=Instance.new("ImageButton")
-OpenButton.Name="OpenButton"
-OpenButton.Size=UDim2.fromOffset(58,58)
-OpenButton.Position=UDim2.new(0.05,0,0.15,0)
-OpenButton.BackgroundColor3=Color3.fromRGB(15,15,20)
-OpenButton.BackgroundTransparency=0.2
-OpenButton.BorderSizePixel=0
-OpenButton.AutoButtonColor=false
-OpenButton.Active=true
-OpenButton.Image=""
-OpenButton.ZIndex=10
-OpenButton.Parent=ScreenGui
+local OpenMenu=Instance.new("ImageButton")
+OpenMenu.Name="OpenMenu"
+OpenMenu.Size=UDim2.fromOffset(58,58)
+OpenMenu.Position=UDim2.new(0.05,0,0.15,0)
+OpenMenu.BackgroundColor3=Color3.fromRGB(15,15,20)
+OpenMenu.BackgroundTransparency=0.2
+OpenMenu.BorderSizePixel=0
+OpenMenu.AutoButtonColor=false
+OpenMenu.Active=true
+OpenMenu.ZIndex=10
+OpenMenu.Parent=ScreenGui
 local OpenCorner=Instance.new("UICorner")
 OpenCorner.CornerRadius=UDim.new(8,0)
-OpenCorner.Parent=OpenButton
+OpenCorner.Parent=OpenMenu
 local OpenStroke=Instance.new("UIStroke")
 OpenStroke.Thickness=2
 OpenStroke.Color=Color3.fromRGB(255,255,255)
-OpenStroke.Parent=OpenButton
+OpenStroke.Parent=OpenMenu
 local OpenGradient=Instance.new("UIGradient")
 OpenGradient.Color=ColorSequence.new({
 ColorSequenceKeypoint.new(0,Color3.fromRGB(255,0,0)),
@@ -34,14 +33,10 @@ Image.Name="Image"
 Image.Size=UDim2.new(0.8,0,0.8,0)
 Image.Position=UDim2.new(0.1,0,0.1,0)
 Image.BackgroundTransparency=1
-Image.BorderSizePixel=0
 Image.Image="rbxassetid://112921115907036"
-Image.ImageTransparency=0
-Image.ImageColor3=Color3.fromRGB(255,255,255)
-Image.ScaleType=Enum.ScaleType.Fit
 Image.Visible=true
-Image.ZIndex=20
-Image.Parent=OpenButton
+Image.ZIndex=11
+Image.Parent=OpenMenu
 local ImageCorner=Instance.new("UICorner")
 ImageCorner.CornerRadius=UDim.new(8,0)
 ImageCorner.Parent=Image
@@ -68,7 +63,7 @@ ColorSequenceKeypoint.new(0,Color3.fromRGB(255,0,0)),
 ColorSequenceKeypoint.new(1,Color3.fromRGB(255,255,0))
 })
 MenuGradient.Parent=MenuStroke
-OpenButton.Activated:Connect(function()
+OpenMenu.Activated:Connect(function()
 MenuFrame.Visible=not MenuFrame.Visible
 end)
 local StrokeRotation=0
