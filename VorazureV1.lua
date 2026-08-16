c0f24c6f534c0583e0d3d535f0373cf841f849e0
HEAVELYNE  ART <zxintrs@gmail.com>
2026-08-16 07:33:51 +0700

    Update VorazureV1.lua
    Signed-off-by: HEAVELYNE  ART <zxintrs@gmail.com>

--- VorazureV1.lua
+++ VorazureV1.lua
@@ -10,12 +10,11 @@ ScreenGui.Parent=game:GetService("CoreGui")
 local OpenMenu=Instance.new("ImageButton")
 OpenMenu.Name="OpenMenu"
 OpenMenu.Size=UDim2.fromOffset(58,58)
-OpenMenu.Position=UDim2.new(0.05,0,0.35,0)
+OpenMenu.Position=UDim2.new(0.05,0,0.15,0)
 OpenMenu.BackgroundColor3=Color3.fromRGB(15,15,20)
 OpenMenu.BackgroundTransparency=0.2
 OpenMenu.BorderSizePixel=0
 OpenMenu.AutoButtonColor=false
-OpenMenu.Active=true
 OpenMenu.ZIndex=10
 OpenMenu.Parent=ScreenGui
 
@@ -30,8 +29,8 @@ OpenStroke.Parent=OpenMenu
 
 local OpenGradient=Instance.new("UIGradient")
 OpenGradient.Color=ColorSequence.new({
-	ColorSequenceKeypoint.new(0,Color3.fromRGB(255,0,0)),
-	ColorSequenceKeypoint.new(1,Color3.fromRGB(255,255,0))
+ColorSequenceKeypoint.new(0,Color3.fromRGB(255,0,0)),
+ColorSequenceKeypoint.new(1,Color3.fromRGB(255,255,0))
 })
 OpenGradient.Parent=OpenStroke
 
@@ -44,13 +43,17 @@ IconImage.Image="rbxassetid://101640388423900"
 IconImage.ZIndex=11
 IconImage.Parent=OpenMenu
 
+local ImageCorner=Instance.new("UICorner")
+ImageCorner.CornerRadius=UDim.new(8,0)
+ImageCorner.Parent=IconImage
+
 local MenuFrame=Instance.new("Frame")
 MenuFrame.Name="MenuFrame"
 MenuFrame.Size=UDim2.fromOffset(650,450)
 MenuFrame.Position=UDim2.new(0.5,-325,0.5,-225)
 MenuFrame.BackgroundColor3=Color3.fromRGB(12,12,18)
 MenuFrame.BorderSizePixel=0
-MenuFrame.Active=true
+MenuFrame.Active=false
 MenuFrame.Visible=false
 MenuFrame.ZIndex=2
 MenuFrame.Parent=ScreenGui
@@ -66,20 +69,20 @@ MenuStroke.Parent=MenuFrame
 
 local MenuGradient=Instance.new("UIGradient")
 MenuGradient.Color=ColorSequence.new({
-	ColorSequenceKeypoint.new(0,Color3.fromRGB(255,0,0)),
-	ColorSequenceKeypoint.new(1,Color3.fromRGB(255,255,0))
+ColorSequenceKeypoint.new(0,Color3.fromRGB(255,0,0)),
+ColorSequenceKeypoint.new(1,Color3.fromRGB(255,255,0))
 })
 MenuGradient.Parent=MenuStroke
 
 OpenMenu.Activated:Connect(function()
-	MenuFrame.Visible=not MenuFrame.Visible
+MenuFrame.Visible=not MenuFrame.Visible
 end)
 
 local StrokeRotation=0
 local StrokeSpeed=45
 
 RunService.RenderStepped:Connect(function(dt)
-	StrokeRotation=(StrokeRotation+StrokeSpeed*dt)%360
-	OpenGradient.Rotation=StrokeRotation
-	MenuGradient.Rotation=StrokeRotation
+StrokeRotation=(StrokeRotation+StrokeSpeed*dt)%360
+OpenGradient.Rotation=StrokeRotation
+MenuGradient.Rotation=StrokeRotation
 end)
