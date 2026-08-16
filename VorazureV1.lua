039451dfc120949bc197735cb87316d7af65bbc1
HEAVELYNE  ART <zxintrs@gmail.com>
2026-08-16 09:42:12 +0700

    Update VorazureV1.lua
    Signed-off-by: HEAVELYNE  ART <zxintrs@gmail.com>

--- VorazureV1.lua
+++ VorazureV1.lua
@@ -1,10 +1,10 @@
 local RunService=game:GetService("RunService")
+local CoreGui=game:GetService("CoreGui")
 local ScreenGui=Instance.new("ScreenGui")
 ScreenGui.Name="VZMenu"
 ScreenGui.ResetOnSpawn=false
 ScreenGui.ZIndexBehavior=Enum.ZIndexBehavior.Sibling
-ScreenGui.Parent=game:GetService("CoreGui")
-
+ScreenGui.Parent=CoreGui
 local OpenMenu=Instance.new("ImageButton")
 OpenMenu.Name="OpenMenu"
 OpenMenu.Size=UDim2.fromOffset(58,58)
@@ -16,36 +16,34 @@ OpenMenu.AutoButtonColor=false
 OpenMenu.Active=true
 OpenMenu.ZIndex=10
 OpenMenu.Parent=ScreenGui
-
 local OpenCorner=Instance.new("UICorner")
 OpenCorner.CornerRadius=UDim.new(8,0)
 OpenCorner.Parent=OpenMenu
-
 local OpenStroke=Instance.new("UIStroke")
 OpenStroke.Thickness=2
 OpenStroke.Color=Color3.fromRGB(255,255,255)
 OpenStroke.Parent=OpenMenu
-
 local OpenGradient=Instance.new("UIGradient")
 OpenGradient.Color=ColorSequence.new({
 ColorSequenceKeypoint.new(0,Color3.fromRGB(255,0,0)),
 ColorSequenceKeypoint.new(1,Color3.fromRGB(255,255,0))
 })
 OpenGradient.Parent=OpenStroke
-
 local Image=Instance.new("ImageLabel")
 Image.Name="Image"
+Image.AnchorPoint=Vector2.new(0.5,0.5)
 Image.Size=UDim2.new(0.8,0,0.8,0)
-Image.Position=UDim2.new(0.1,0,0.1,0)
+Image.Position=UDim2.new(0.5,0,0.5,0)
 Image.BackgroundTransparency=1
 Image.Image="rbxassetid://101640388423900"
+Image.ImageTransparency=0
+Image.Visible=true
+Image.Rotation=0
 Image.ZIndex=11
 Image.Parent=OpenMenu
-
 local ImageCorner=Instance.new("UICorner")
 ImageCorner.CornerRadius=UDim.new(8,0)
 ImageCorner.Parent=Image
-
 local MenuFrame=Instance.new("Frame")
 MenuFrame.Name="MenuFrame"
 MenuFrame.Size=UDim2.fromOffset(650,450)
@@ -56,16 +54,13 @@ MenuFrame.Active=false
 MenuFrame.Visible=false
 MenuFrame.ZIndex=2
 MenuFrame.Parent=ScreenGui
-
 local MenuCorner=Instance.new("UICorner")
 MenuCorner.CornerRadius=UDim.new(0,8)
 MenuCorner.Parent=MenuFrame
-
 local MenuStroke=Instance.new("UIStroke")
 MenuStroke.Thickness=2
 MenuStroke.Color=Color3.fromRGB(255,255,255)
 MenuStroke.Parent=MenuFrame
-
 local MenuGradient=Instance.new("UIGradient")
 MenuGradient.Color=ColorSequence.new({
 ColorSequenceKeypoint.new(0,Color3.fromRGB(255,0,0)),
@@ -75,11 +70,14 @@ MenuGradient.Parent=MenuStroke
 OpenMenu.Activated:Connect(function()
 MenuFrame.Visible=not MenuFrame.Visible
 end)
-
 local StrokeRotation=0
+local ImageRotation=0
 local StrokeSpeed=45
+local ImageSpeed=45
 RunService.RenderStepped:Connect(function(dt)
 StrokeRotation=(StrokeRotation+StrokeSpeed*dt)%360
+ImageRotation=(ImageRotation+ImageSpeed*dt)%360
 OpenGradient.Rotation=StrokeRotation
 MenuGradient.Rotation=StrokeRotation
+Image.Rotation=ImageRotation
 end)
